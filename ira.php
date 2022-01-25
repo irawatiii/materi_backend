c9f5e2d614d66f5a3a0b9eb57374ceffdeeeec94
irawatiii <97495509+irawatiii@users.noreply.github.com>
2022-01-25 16:20:20 +0700

    Add files via upload

---
+++ ira.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+// variabel = $d
+// value = 2500
+$d = 25000;
+if ($d == "2500") {
+	echo "untung";
+} else if ($d < "2500") {
+	echo "rugi";
+} else{
+	echo "SUKSES";
+}
+?>  
+
+</body>
+</html>
